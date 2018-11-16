c44c6604ed67add39d0147aed3322f0e149ecb42
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-16 16:56:44 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -12,9 +12,9 @@ function ninjaBabySelector() {
 }
 
 function divSelector() {
-  return $('div .pics')
+  return $('div.pics')
 }
 
 function firstListItem() {
-  return $('ul:first-child #pic-list')
+  return $('ul:first-child#pic-list')
 }
